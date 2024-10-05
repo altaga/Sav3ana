6dfdccc62565ec05e67cf8904a4f025a9564e2d6
altaga <v.a.i@hotmail.com>
2024-10-04 23:29:03 -0600

    commit

--- Sav3ana/src/screens/paymentWallet/components/readCard.js
+++ Sav3ana/src/screens/paymentWallet/components/readCard.js
@@ -1,70 +1,75 @@
-import {Text, View} from 'react-native';
+import {Text, View, ToastAndroid} from 'react-native';
 import React, {Component} from 'react';
 import NfcManager, {NfcTech} from 'react-native-nfc-manager';
 import emv from 'node-emv';
 import IconFAB from 'react-native-vector-icons/FontAwesome6';
 import GlobalStyles from '../../../styles/styles';
 
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 export default class ReadCard extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      red: false,
+      reading: false,
+    };
+    this.interval = null;
   }
   readVisaCreditCard = async () => {
-    console.log('reading Visa credit card...');
-    try {
-      NfcManager.cancelTechnologyRequest();
-    } catch (error) {
-      console.warn(error);
-    }
-
-    try {
-      const commands = [
-        '00A404000E325041592E5359532E444446303100',
-        '00A4040007A000000003101000',
-        '80A80000238321F620C00000000000000100000000000007240000000000097823112300194E172C00',
-        '80A800002383212800000000000000000000000000000002500000000000097820052600E8DA935200',
-        '80CA9F1700',
-        '80CA9F3600',
-      ];
-
-      await NfcManager.requestTechnology([NfcTech.IsoDep]);
-
-      const responses = [];
-
-      for (let i = 0; i < commands.length; i++) {
-        const resp = await NfcManager.isoDepHandler.transceive(
-          this.toByteArray(commands[i]),
-        );
-        responses.push(resp);
+    return new Promise(async resolve => {
+      console.log('reading Visa credit card...');
+      try {
+        // cancel any previous requests
+        await NfcManager.cancelTechnologyRequest();
+      } catch (error) {
+        // do nothing
       }
+      try {
+        const commands = [
+          '00A404000E325041592E5359532E444446303100',
+          '00A4040007A000000003101000',
+          '80A80000238321F620C00000000000000100000000000007240000000000097823112300194E172C00',
+          '80A800002383212800000000000000000000000000000002500000000000097820052600E8DA935200',
+          '80CA9F1700',
+          '80CA9F3600',
+        ];
 
-      if (responses && responses.length > 2) {
-        const r = await this.getEmvInfo(this.toHexString(responses[2]));
-        if (r) {
-          const cardInfo = this.getCardInfoVisa(r);
-          if (cardInfo) {
-            return {
-              card: cardInfo.card,
-              exp: cardInfo.exp,
-            };
+        await NfcManager.requestTechnology([NfcTech.IsoDep]);
+
+        const responses = [];
+
+        for (let i = 0; i < commands.length; i++) {
+          const resp = await NfcManager.isoDepHandler.transceive(
+            this.toByteArray(commands[i]),
+          );
+          responses.push(resp);
+        }
+
+        if (responses && responses.length > 2) {
+          const r = await this.getEmvInfo(this.toHexString(responses[2]));
+          if (r) {
+            const cardInfo = this.getCardInfoVisa(r);
+            if (cardInfo) {
+              resolve({
+                card: cardInfo.card,
+                exp: cardInfo.exp,
+              });
+            } else {
+              resolve(null);
+            }
           } else {
-            console.warn('Card info not found 1');
-            return null;
+            resolve(null);
           }
         } else {
-          console.warn('Card info not found 2');
-          return null;
+          resolve(null);
         }
-      } else {
-        console.warn('Card info not found 3');
-        return null;
+      } catch (error) {
+        resolve(null);
       }
-    } catch (error) {
-      //console.warn("Error:",error);
-      return null;
-    } finally {
-      NfcManager.cancelTechnologyRequest();
-    }
+    });
   };
 
   getEmvInfo = info => {
@@ -124,19 +129,50 @@ export default class ReadCard extends Component {
   };
 
   async componentDidMount() {
-    const cardInfo = await this.readVisaCreditCard();
-    this.props.cardInfo(cardInfo);
+    this.interval = setInterval(async () => {
+      if(this.state.reading) {
+        console.log('Reading...');
+        return;
+      };
+      this.setStateAsync({reading: true}); 
+      const cardInfo = await this.readVisaCreditCard();
+      if (cardInfo !== null) {
+        this.setState({red: true});
+        this.props.cardInfo(cardInfo);
+        clearInterval(this.interval);
+        this.interval = null;
+      } else {
+        console.log('No card found');
+        ToastAndroid.show('Read again', ToastAndroid.SHORT);
+        await this.setStateAsync({reading: false});
+      }
+    }, 1000);
   }
 
-  componentWillUnmount() {
-    NfcManager.cancelTechnologyRequest();
+  async componentWillUnmount() {
+    this.interval && clearInterval(this.interval);
+  }
+
+  async setStateAsync(value) {
+    return new Promise(resolve => {
+      this.setState(
+        {
+          ...value,
+        },
+        () => resolve(),
+      );
+    });
   }
 
   render() {
     return (
       <View style={{justifyContent: 'center', alignItems: 'center'}}>
-        <IconFAB name="nfc-symbol" size={240} color="white" />
-        <Text style={GlobalStyles.title}>Tap to pay</Text>
+        <IconFAB
+          name="nfc-symbol"
+          size={240}
+          color={this.state.red ? 'white' : 'gray'}
+        />
+        <Text style={GlobalStyles.title}>Tap the NFC reader</Text>
       </View>
     );
   }
